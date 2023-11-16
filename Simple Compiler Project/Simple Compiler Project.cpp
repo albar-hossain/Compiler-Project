64e62443608567b443a51d54b0cb56d11ffefb70
Albar Hossain <albarhossain@gmail.com>
2023-11-16 14:06:27 +0600

    Improved lexical analyzer and fixed bugs

--- Simple Compiler Project/Simple Compiler Project.cpp
+++ Simple Compiler Project/Simple Compiler Project.cpp
@@ -5,40 +5,82 @@
 using namespace std;
 
 int main() {
-    string input;
-    cout << "Enter a string: ";
-    getline(cin, input);
+	string input;
+	cout << "Enter a string: ";
+	getline(cin, input);
 
-    string token = "";
+	string token = "";
+	int count = 0;
+	for (char c : input) {
+		if (isalnum(c)) {
+			// Identifier or Number token
+			token += c;
+		}
+		else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '=' || c == ';') {
+			// Operator or Semicolon token
+			if (!token.empty()) {
+				// Check if the token is a number
+				bool isNumber = true;
+				for (char tc : token) {
+					if (!isdigit(tc)) {
+						isNumber = false;
+						break;
+					}
+				}
 
-    for (char c : input) {
-        if (isalnum(c)) {
-            // Identifier or Number token
-            token += c;
-        }
-        else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == ';') {
-            // Operator or Semicolon token
-            if (!token.empty()) {
-                cout << "<" << token <<">";
-                token = "";
-            }
-            cout << "<" << c << ">";
-        }
-        else if (!isspace(c)) {
-            // Invalid token
-            continue;
-        }
-        else if (!token.empty()) {
-            // Output Identifier or Number token if any
-            cout << "<" << token << ">";
-            token = "";
-        }
-    }
+				if (isNumber) {
+					cout << "<" << token << ">";
+				}
+				else {
+					cout << "<id," << count++ << ">";
+				}
+				token = "";
+			}
+			cout << "<" << c << ">";
+		}
+		else if (!isspace(c)) {
+			// Invalid token
+			cout << "Input contains invalid token: " << c << endl;
+			return 1; // Exit with an error code
+		}
+		else if (!token.empty()) {
+			// Output Identifier or Number token if any
+			bool isNumber = true;
+			for (char tc : token) {
+				if (!isdigit(tc)) {
+					isNumber = false;
+					break;
+				}
+			}
 
-    if (!token.empty()) {
-        // Output any remaining Identifier or Number token
-        cout << "<" << token << ">";
-    }
+			if (isNumber) {
+				cout << "<" << token << ">";
+			}
+			else {
+				cout << "<id," << count++ << ">";
+			}
+			token = "";
+		}
+	}
 
-    return 0;
+	if (!token.empty()) {
+		// Output any remaining Identifier or Number token
+		// Check if the token is a number
+		bool isNumber = true;
+		for (char tc : token) {
+			if (!isdigit(tc)) {
+				isNumber = false;
+				break;
+			}
+		}
+
+		if (isNumber) {
+			cout << "<" << token << ">";
+		}
+		else {
+			cout << "<id," << count++ << ">";
+		}
+	}
+
+	return 0;
 }
