668bb895ffd9bb927bf9e2ab13d4696b91f4b103
Albar Hossain <albarhossain@gmail.com>
2023-11-21 22:13:38 +0600

    Working on syntax tree

--- Simple Compiler Project/Simple Compiler Project.cpp
+++ Simple Compiler Project/Simple Compiler Project.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <string.h>
 #include <string>
 using namespace std;
 
 int main() {
-	int syntax_tree_root, start_variable, top_rule, left_variable;
+	int syntax_tree_root = 0, start_variable = 0, top_rule = 0, left_variable = 0;
 	int input_token_index, statement_index;
 	string input_token;
-	char statement[7];
+	string syntax_tree_token;
+	char statement[7] ="";
+
 
 	char grammar_rules[6][11] = { "A->BCDEDF.","B->i.","C->=.","D->i.","E->+.","F->;." };
 
-	char variable_replacer[50];
-	char syntax_tree[50];
-	int children_counter[50];
+	char variable_replacer[50] = {0};
+	char syntax_tree[50] = "";
+	int children_counter[50] = { 0 };  // Initialize children_counter array with zeros.
 
 	cout << "Enter the input token:";
 	getline(cin, input_token);
@@ -28,9 +29,12 @@ int main() {
 			statement_index++;
 		}
 	}
-
-	//for(statement_index=0;statement_index<7;statement_index++)
-	  //cout<<statement[statement_index];
+	string statement_token;
+	for (int i = 0; i < 6; i++)
+	{
+		statement_token += statement[i];
+	}
+	cout << "The generated statement is:" << statement_token << endl;
 
 	syntax_tree_root = 0;
 	start_variable = 0;
@@ -41,23 +45,22 @@ int main() {
 
 	variable_replacer[start_variable] = grammar_rules[top_rule][left_variable];
 
-	int varaiable_replacer_current_index = 0;
+	int variable_replacer_current_index = 0;
 	int syntax_tree_current_index = 1;
 	int statement_matcher_index = 0;
 
-	//loop until all the leaf nodes in the syntax tree are terminals
+	// loop until all the leaf nodes in the syntax tree are terminals
 	while (true) {
-		//find out the rule related to the variable in the variable variable_replacer
 		int grammar_rule_number, rule_right_side_copier_index, copy_flag, parent;
 
 		for (grammar_rule_number = 0; grammar_rule_number < 6; grammar_rule_number++) {
-			if (grammar_rules[grammar_rule_number][left_variable] == variable_replacer[varaiable_replacer_current_index]) {
+			if (grammar_rules[grammar_rule_number][left_variable] == variable_replacer[variable_replacer_current_index]) {
 				copy_flag = 0;
 				parent = syntax_tree_current_index - 1;
 
-				for (rule_right_side_copier_index = 0, grammar_rules[grammar_rule_number][rule_right_side_copier_index] != '.'; rule_right_side_copier_index++;) {
+				for (rule_right_side_copier_index = 3; grammar_rules[grammar_rule_number][rule_right_side_copier_index] != '.'; rule_right_side_copier_index++) {
 					if (grammar_rules[grammar_rule_number][rule_right_side_copier_index] == '>') {
-						copy_flag == 1;
+						copy_flag = 1;
 						continue;
 					}
 
@@ -72,28 +75,41 @@ int main() {
 			}
 		}
 
-		varaiable_replacer_current_index = 0;
+
+		for (int i = 0; i < 6; i++)
+		{
+			syntax_tree_token += syntax_tree[i];
+		}
+
+
+		variable_replacer_current_index = 0;
 
 		if (variable_replacer[variable_replacer_current_index] == 'i' || variable_replacer[variable_replacer_current_index] == '=' || variable_replacer[variable_replacer_current_index] == '+' || variable_replacer[variable_replacer_current_index] == ';') {
 			if (variable_replacer[variable_replacer_current_index] == statement[statement_matcher_index]) {
 				variable_replacer_current_index++;
+				statement_matcher_index++;
 			}
-
-			else
-				cout << "Syntax Error";
+			else {
+				cout << "Syntax Error" << endl;
+				break;
+			}
 		}
-		else
+		else {
 			variable_replacer_current_index++;
+		}
 
 		int i, break_flag = 1;
 		for (i = 0; i < 50; i++) {
-			if (variable_replacer[i] != 'i' && variable_replacer[i] != '=' && variable_replacer[i] != '+' && variable_replacer[i] != ';')
+			if (variable_replacer[i] != 'i' && variable_replacer[i] != '=' && variable_replacer[i] != '+' && variable_replacer[i] != ';') {
 				break_flag = 0;
+			}
 		}
 
-		if (break_flag == 1)
+		if (break_flag == 1) {
 			break;
+		}
 	}
+	cout << "The generated syntax tree is:" << syntax_tree_token << endl;
 
 	return 0;
 }
